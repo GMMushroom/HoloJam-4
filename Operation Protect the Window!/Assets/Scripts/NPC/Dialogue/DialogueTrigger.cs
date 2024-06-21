0584e91aca3ac6c13f730b3a877e8b9eeee38c9b
OverLoad <kotofeykin2000@bk.ru>
2024-06-21 18:03:36 +0300

    Fix: New Dialogue system
    Dialogue system based on dialogue tags, I also added ability to create answer buttons

--- Operation Protect the Window!/Assets/Scripts/NPC/Dialogue/DialogueTrigger.cs
+++ Operation Protect the Window!/Assets/Scripts/NPC/Dialogue/DialogueTrigger.cs
@@ -1,40 +1,126 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using TMPro;
 using UnityEngine;
- 
+using UnityEngine.UI;
+
+#region Data Classes
+
 [System.Serializable]
 public class DialogueCharacter
 {
     public string name;
     public Sprite icon;
 }
- 
+
 [System.Serializable]
-public class DialogueLine
+public class DialogueOption
 {
-    public DialogueCharacter character;
-    [TextArea(3, 10)]
-    public string line;
+    public string optionText;
+    public string nextDialogueTag;
 }
- 
+
 [System.Serializable]
 public class Dialogue
 {
-    public List<DialogueLine> dialogueLines = new List<DialogueLine>();
+    public DialogueCharacter dialogueCharacter;
+    public string dialogueTag;
+    [TextArea(3, 10)]
+    public string dialogueText;
+    public DialogueOption[] options;
+    public string nextDialogueTag;
 }
- 
+
+#endregion
+
 public class DialogueTrigger : MonoBehaviour
 {
-    public Dialogue dialogue;
+    public Dialogue[] dialogues;
+    public GameObject dialoguePanel;
+    public TextMeshProUGUI dialogueText;
+    public Button[] optionButtons;
+    public Button continueButton;
+
+    private Dialogue _currentDialogue;
+    
     public GameObject hint;
 
     private bool _isInZone = false;
-    public void TriggerDialogue()
+
+    private void Start()
     {
-        DialogueManager.Instance.dialoguePanel.SetActive(true);
-        DialogueManager.Instance.StartDialogue(dialogue);
+        _currentDialogue = dialogues.Length > 0 ? dialogues[0] : null;
+        dialoguePanel.SetActive(false);
     }
+    
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && _isInZone)
+        {
+            TriggerDialogue();
+        }
+    }
+
+    private void TriggerDialogue()
+    {
+        if (_currentDialogue != null)
+        {
+            dialoguePanel.SetActive(true);
+            dialogueText.text = _currentDialogue.dialogueText;
+
+            int optionCnt = _currentDialogue.options.Length;
+            for (int i = 0; i < optionButtons.Length; i++)
+            {
+                if (i < optionCnt)
+                {
+                    optionButtons[i].gameObject.SetActive(true);
+                    optionButtons[i].GetComponentInChildren<TextMeshProUGUI>().text = _currentDialogue.options[i].optionText;
+                    int index = i;
+                    optionButtons[i].onClick.RemoveAllListeners();
+                    optionButtons[i].onClick.AddListener(() => SelectOption(index));
+                }
+                else
+                {
+                    optionButtons[i].gameObject.SetActive(false);
+                }
+            }
+            
+            continueButton.gameObject.SetActive(optionCnt == 0);
+            continueButton.onClick.RemoveAllListeners();
+            continueButton.onClick.AddListener(ContinueDialogue);
+        }
+    }
+
+    public void SelectOption(int optionIndex)
+    {
+        if (optionIndex >= 0 && optionIndex <= _currentDialogue.options.Length)
+        {
+            string nextTag = _currentDialogue.options[optionIndex].nextDialogueTag;
+            _currentDialogue = System.Array.Find(dialogues, d => d.dialogueTag == nextTag);
+            TriggerDialogue();
+        }
+    }
+
+    public void ContinueDialogue()
+    {
+        if (string.IsNullOrEmpty(_currentDialogue.nextDialogueTag))
+        {
+            EndDialogue();
+        }
+        else
+        {
+            _currentDialogue = System.Array.Find(dialogues, d => d.dialogueTag == _currentDialogue.nextDialogueTag);
+            TriggerDialogue();
+        }
+    }
+
+    private void EndDialogue()
+    {
+        dialoguePanel.SetActive(false);
+    }
+
+    #region Collider Triggers
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -54,19 +140,5 @@ public class DialogueTrigger : MonoBehaviour
         }
     }
 
-    private void Update()
-    {
-        if (Input.GetKey(KeyCode.E) && !DialogueManager.Instance.isDialogueActive && _isInZone)
-        {
-            TriggerDialogue();
-        }
-    }
-    
-    // private void OnTriggerStay2D(Collider2D collision)
-    // {
-    //     if(collision.tag == "Player" && Input.GetKey(KeyCode.E))
-    //     {
-    //         TriggerDialogue();
-    //     }
-    // }
+    #endregion
 }
